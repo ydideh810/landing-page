143f1b377437351ce47fdb0b9885f5fa79b74793
ydideh810 <yd.baller810@gmail.com>
2024-06-22 00:28:28 -0400

    Update Navbar.tsx

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <div className="sticky flex h-16 w-full place-content-center place-items-center border-b border-zinc-200 px-8 shadow-sm sm:h-24 lg:px-16">
       <div className="flex w-full max-w-screen-xl place-content-between">
         <Link className="flex place-items-center gap-2" to={"/"}>
-          <img src={logo} alt="Jouzu logo" className="h-8" />
+          <img src={logo} alt="Niddam logo" className="h-8" />
           <div className="font-poppins text-2xl font-normal tracking-tighter">Niddam</div>
         </Link>
       </div>
