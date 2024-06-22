5aeb30eeecc41064a8f3184cd40af45d362127c6
ydideh810 <yd.baller810@gmail.com>
2024-06-22 00:25:19 -0400

    Update Footer.tsx

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,4 +1,4 @@
-import logo from "/nid.svg";
+import logo from "/final.svg";
 
 function Footer() {
   return (
@@ -6,7 +6,7 @@ function Footer() {
       <div className="flex w-full max-w-screen-xl place-content-between place-items-center">
         <img
           src={logo}
-          alt="Jouzu logo"
+          alt="Niddam logo"
           className="h-8 grayscale filter duration-150 ease-in-out hover:filter-none"
         />
         <span className="text-xs text-zinc-400">© 2024 Niddam</span>
