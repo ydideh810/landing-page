703a6f1f534dd5872884983c4bc2c4c4fc34ea94
ydideh810 <yd.baller810@gmail.com>
2024-06-22 23:38:36 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from "@/pages/home";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 import NavbarAndFooter from "./components/layouts/NavbarAndFooter";
+import PillButton, HoverPillButton from "./components/ui/WaitlistInput";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+       <PillButton />
+      <HoverPillButton/>
     </NavbarAndFooter>
   );
 }
