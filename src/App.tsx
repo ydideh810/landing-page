71219e7415b4e71d47698cc9bf968996c3e7377e
ydideh810 <yd.baller810@gmail.com>
2024-06-23 11:47:05 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import Home from "@/pages/home";
 import { Route, Routes, Navigate } from "react-router-dom";
 import NavbarAndFooter from "./components/layouts/NavbarAndFooter";
-import PillButton from "./components/ui/WaitlistInput";
-import HoverPillButton from "./components/ui/WaitlistInput";
 
 function App() {
   return (
-<div>
     <NavbarAndFooter>
       <Routes>
         {/* Single catch-all route because we only have one page now */}
@@ -14,9 +11,6 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </NavbarAndFooter>
-     <PillButton />
-      <HoverPillButton/>
-  </div>
   );
 }
 
