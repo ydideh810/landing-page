b718de312a2e3b736210351641413c3b84d60cb0
ydideh810 <yd.baller810@gmail.com>
2024-06-22 22:35:49 -0400

    Update WaitlistInput.tsx

--- src/components/ui/WaitlistInput.tsx
+++ src/components/ui/WaitlistInput.tsx
@@ -88,5 +88,7 @@ export default function WaitlistInput() {
         onChange={(e) => setEmail(e.target.value)}
       />
     </form>
-  );
+    
+   <a href="https://www.niddamyar.online/" style="background-color: #000; color: #fff; font-family: Arial, sans-serif; font-size: 18px; font-weight: bold; padding: 10px 20px; border-radius: 50%;  text-align: center; text-decoration: none; transition: background-color 0.3s, color 0.3s;">EXPLORE</a>
+    );
 }
