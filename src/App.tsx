48ddf5b5ff96c5dcb6cf8a7179622f8d5c1ad995
ydideh810 <yd.baller810@gmail.com>
2024-06-23 00:21:59 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import HoverPillButton from "./components/ui/WaitlistInput";
 
 function App() {
   return (
-
+<div>
     <NavbarAndFooter>
       <Routes>
         {/* Single catch-all route because we only have one page now */}
@@ -16,7 +16,7 @@ function App() {
     </NavbarAndFooter>
      <PillButton />
       <HoverPillButton/>
-
+  </div>
   );
 }
 
